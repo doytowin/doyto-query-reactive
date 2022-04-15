19b532d826087985b38816daec806b43ce7ddf1a
Forb Yuan <f0rb@163.com>
2022-04-15 22:05:00 +0800

    [doyto-query-webflux] RoleWebfluxTest#shouldReturnEmptyData fix NPE in JsonBodyHandler

--- doyto-query-webflux/src/test/java/win/doyto/query/web/demo/module/role/RoleController.java
+++ doyto-query-webflux/src/test/java/win/doyto/query/web/demo/module/role/RoleController.java
@@ -1,17 +1,43 @@
+/*
+ * Copyright © 2019-2022 Forb Yuan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package win.doyto.query.web.demo.module.role;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 import win.doyto.query.reactive.webflux.controller.ReactiveEIQController;
 import win.doyto.query.test.role.RoleEntity;
 import win.doyto.query.test.role.RoleQuery;
+import win.doyto.query.web.response.JsonBody;
 
 /**
  * RoleController
  *
  * @author f0rb on 2021-10-26
  */
+@JsonBody
 @RestController
 @RequestMapping("role")
 public class RoleController extends ReactiveEIQController<RoleEntity, Integer, RoleQuery> {
+
+    @GetMapping("void")
+    public Mono<?> voidType() {
+        return Mono.empty();
+    }
+
 }
--- doyto-query-webflux/src/test/java/win/doyto/query/web/demo/module/role/RoleWebfluxTest.java
+++ doyto-query-webflux/src/test/java/win/doyto/query/web/demo/module/role/RoleWebfluxTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2019-2022 Forb Yuan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package win.doyto.query.web.demo.module.role;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -127,5 +143,15 @@ class RoleWebfluxTest {
                      .jsonPath("$.data.roleName").isEqualTo("vip0")
                      .jsonPath("$.data.roleCode").isEqualTo("VIP0")
                      .jsonPath("$.data.valid").isEqualTo(true);
+    }
+
+    @Test
+    void shouldReturnEmptyData() {
+        webTestClient.get().uri("/role/void")
+                     .exchange()
+                     .expectBody()
+                     .consumeWith(log())
+                     .jsonPath("$.success").isEqualTo(true)
+                     .jsonPath("$.data").doesNotExist();
     }
 }
